a123a589e7d7c98a4591cd3f62329b057ed2dc8c
dimitrilavin <dlavin@live.unc.edu>
2022-03-08 13:03:19 -0500

    chrome extension

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,5 +2,5 @@ let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log('Background color set to %cgreen', `color: ${color}`);
 });
